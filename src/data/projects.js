0d072e459b795a9595971abe6c00401e878ed68e
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2022-12-19 16:42:50 +0100

    init

---
+++ src/data/projects.js
@@ -0,0 +1,62 @@
+// Projects data.
+
+const projects = [
+	{
+		id: 1,
+		title: 'Songbird Domains',
+		category: 'Utility',
+		img: require('@/assets/images/projects/songbird-domains.jpg'),
+		url: 'https://songbird.domains'
+	},
+	{
+		id: 2,
+		title: 'The Sparktans',
+		category: 'NFT',
+		img: require('@/assets/images/projects/sparktans.jpeg'),
+		url: 'https://songbird.domains'
+	},
+	{
+		id: 3,
+		title: 'Bifrost Wallet',
+		category: 'Wallet',
+		img: require('@/assets/images/projects/bifrost.png'),
+		url: 'https://songbird.domains'
+	},
+	{
+		id: 4,
+		title: 'Flare Oracle',
+		category: 'FTSO',
+		img: require('@/assets/images/projects/satraps.png'),
+		url: 'https://songbird.domains'
+	},
+	{
+		id: 5,
+		title: 'The Satraps',
+		category: 'NFT',
+		img: require('@/assets/images/projects/satraps.png'),
+		url: 'https://songbird.domains'
+	},
+	{
+		id: 6,
+		title: 'Pangolin',
+		category: 'DeFi',
+		img: require('@/assets/images/projects/satraps.png'),
+		url: 'https://songbird.domains'
+	},
+	{
+		id: 7,
+		title: 'Satrap IDs',
+		category: 'Utility',
+		img: require('@/assets/images/projects/satraps.png'),
+		url: 'https://songbird.domains'
+	},
+	{
+		id: 8,
+		title: 'Spark Coin',
+		category: 'Utility',
+		img: require('@/assets/images/projects/satraps.png'),
+		url: 'https://songbird.domains'
+	},
+];
+
+export default projects;
